8245b314edebb101f3e7b87434e454b06d59febc
HcDuller <henriqueduller@hotmail.com>
2023-02-28 19:51:15 +0100

    added void casting to dynamic cast to get rid of -Wunused-value warning

--- ex02/identify.cpp
+++ ex02/identify.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   identify.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:47:24 by hde-camp          #+#    #+#             */
-/*   Updated: 2023/02/28 15:38:56 by hde-camp         ###   ########.fr       */
+/*   Updated: 2023/02/28 19:50:16 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,20 @@ void identify(Base& p){
 	bool unidentified = true;
 	std::cout << "Base& p actual type: ";
 	try{
-		dynamic_cast<A&>(p);
+		(void)dynamic_cast<A&>(p);
 		std::cout << "A&." << std::endl;
 		unidentified = false;
 	}catch(std::exception& e){(void)e;}
 	if (unidentified){
 		try{
-			dynamic_cast<B&>(p);
+			(void)dynamic_cast<B&>(p);
 			std::cout << "B&." << std::endl;
 			unidentified = false;
 		}catch(std::exception& e){(void)e;}
 	}
 	if (unidentified){
 		try{
-			dynamic_cast<C&>(p);
+			(void)dynamic_cast<C&>(p);
 			std::cout << "C&." << std::endl;
 			unidentified = false;
 		}catch(std::exception& e){
